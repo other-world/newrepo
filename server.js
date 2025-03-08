bd1a9240a884155863d692470c3aa56869ffa00b
other-world <jamie@other-world.org>
2025-03-07 21:10:56 -0700

    Latest updates to our week 01 exercise.

--- server.js
+++ server.js
@@ -2,23 +2,36 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
+
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
+/* ************************** *
+ * View Engine and Templates  *
+ * ************************** */
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+/* ********************* *
+ *        Routes         *
+ * ********************* */
+app.use(static)
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
+
+/* ************************************ *
+ *       Local Server Information       *
+ * Values from .env (environment) file  *
+ * ************************************ */
 const port = process.env.PORT
 const host = process.env.HOST
 
