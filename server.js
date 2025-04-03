4ba6f19eb88a6dbc6f70f761fd0549071f5f70f7
other-world <jamie@other-world.org>
2025-04-03 07:51:39 -0500

    Lots of changes adding functionality to the inventory pages.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 const session = require("express-session")
 const pool = require('./database/')
@@ -53,6 +54,9 @@ app.get("/", baseController.buildHome)
 //app.get("/", function(req, res){
 //  res.render("index", {title: "Home"})
 //})
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ************************************ *
  *       Local Server Information       *
